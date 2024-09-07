52a092c5b2e664d5f345cb0237b54c0b9e300079
FelipePolinarski <134339267+FelipePolinarski@users.noreply.github.com>
2024-09-07 10:22:35 -0300

    Create lista5.js

---
+++ lista5.js
@@ -0,0 +1,93 @@
+let Carro = {
+  marca: "Fiat",
+  cor: "Prata",
+  nome: "Uno",
+  valor: "R$ 20.000,00"
+};
+
+
+
+let Pessoa = {
+  nome: "Felipe",
+  cpf: "123.456.789-00",
+  rg: "12.345.678-9",
+  idade: 17
+};
+
+
+
+let Aluno = {
+  nome: "Felipe",
+  cpf: "123.456.789-00",
+  idade: 17,
+  turma; "3°A"
+}
+
+
+
+let Fruta = {
+  nome: "Abacaxi",
+  cor: "Amarelo",
+  tipo: "Cítrica",
+  peso: "1kg"
+}
+
+
+
+let ListaPessoas = [
+  {nome: "Felipe", cpf: "123.456.789-00", rg: "12.345.678-9", idade: 17},
+  {nome: "Decio", cpf: "987.654.321-00", rg: "98.765.432-1", idade: 60},
+  {nome: "Eliane", cpf: "135.791.356-00", rg: "13.579.135-7", idade: 53},
+  {nome: "Fernando", cpf: "246.824.680-00", rg: "24.680.246-0", idade: 32},
+  {nome: "Fabiana", cpf: "112.134.151-60", rg:"14.151.617-9", idade: 31}
+]
+
+
+
+let ListaAnimais = [
+  {nome: "Tigre", peso: "90kg", altura: "1.90m"},
+  {nome: "Macaco", peso: "15kg", altura: "0.50m"},
+  {nome: "Gato", peso: "5kg", altura: "0.30m"},
+  {nome: "Cachorro", peso: "15kg", altura: "0.70m"},
+  {nome: "Panda", peso: "150kg", altura: "1.50m"}
+]
+
+
+
+let ListaDisciplinas = [
+  {nome: "Matemática", codigo: "MAT", turma: "3°A"},
+  {nome: "Português", codigo: "POR", turma: "3°B"},
+  {nome: "História", codigo: "HIS", turma: "3°C"},
+  {nome: "Geografia", codigo: "GEO", turma: "3°D"},
+  {nome: "Química", codigo: "QUI", turma: "3°E"}
+]
+
+
+
+let ListaProfessores = [
+  {nome: "Andreia", disciplina: "Matemática", cpf: "123.456.789-00"},
+  {nome: "Claudinei", disciplina: "Português", cpf: "987.654.321-00"},
+  {nome: "Maurício", disciplina: "História", cpf: "135.791.356-00"},
+  {nome: "Frida", disciplina: "Geografia", cpf: "246.824.680-00"},
+  {nome: "Ticiane", disciplina: "Química", cpf: "112.134.151-60"}
+]
+
+
+
+let ListaCasas = [
+  {rua: "Rua A", tamanho: "100m²", bairro: "Bairro A"},
+  {rua: "Rua B", tamanho: "200m²", bairro: "Bairro B"},
+  {rua: "Rua C", tamanho: "300m²", bairro: "Bairro C"},
+  {rua: "Rua D", tamanho: "400m²", bairro: "Bairro D"},
+  {rua: "Rua E", tamanho: "500m²", bairro: "Bairro E"}
+]
+
+
+
+let ListaVegetais = [
+  {nome: "batata", tipo: "legume", valor: "R$ 5,00"},
+  {nome: "cenoura", tipo: "legume", valor: "R$ 3,00"},
+  {nome: "beterraba", tipo: "legume", valor: "R$ 4,00"},
+  {nome: "alface", tipo: "legume", valor: "R$ 2,00"},
+  {nome: "tomate", tipo: "legume/fruta", valor: "R$ 3,00"}
+]
